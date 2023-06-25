0284549a2575b02fa19ffe7a2f33dd004319c9d1
pontusj101 <pontusj@kth.se>
2023-06-25 13:21:50 +0000

    Before major refactoring.

--- mal_sampler.py
+++ mal_sampler.py
@@ -1,9 +1,15 @@
 from scipy.stats import rv_discrete, bernoulli, binom
 import random
 
+# For all possible actions, compute likelihood of resulting model. 
+# Choose action that maximizes likelihood.
+
 class Asset:
     def __init__(self, name: str):
         self.name = name
+
+    def fully_associated(self, associated_asset_type: str):
+        return True
 
     def print(self):
         print(self.name)
@@ -12,29 +18,61 @@ class Network(Asset):
     def __init__(self, name: str):
         super().__init__(name)
         self.n_associated_networks = binom(n=5, p=0.5).rvs()
+        self.n_unassociated_networks = binom(n=10, p=0.5).rvs()
         self.associated_networks = set()
+        self.n_associated_hosts = binom(n=10, p=0.5).rvs()
+        self.associated_hosts = set()
+
+    def fully_associated(self, associated_asset_type: str):
+        if associated_asset_type == 'network':
+            return len(self.associated_networks) == self.n_associated_networks
+        if associated_asset_type == 'host':
+            return len(self.associated_hosts) == self.n_associated_hosts
+        raise ValueError(f'Unknown associated asset type: {associated_asset_type}')
+
+    def vote_for_new_network(self, n_networks: int):
+        current_associated_networks = len(self.associated_networks)
+        current_unassociated_networks = n_networks - current_associated_networks
+        target_associated_networks = self.n_associated_networks
+        target_unassociated_networks = self.n_unassociated_networks
+
+
 
     def print(self):
         print(f'Network: {self.name}. Number of associated networks: {self.n_associated_networks}, Associated networks: {[an.name for an in self.associated_networks]}')
 
+class Host(Asset):
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.n_associated_networks = 1
+        self.associated_networks = set()
+
+    def fully_associated(self, associated_asset_type: str):
+        if associated_asset_type == 'network':
+            return len(self.associated_networks) == self.n_associated_networks
+        raise ValueError(f'Unknown associated asset type: {associated_asset_type}')
+
+    def print(self):
+        print(f'Host: {self.name}. Number of associated networks: {self.n_associated_networks}, Associated networks: {[an.name for an in self.associated_networks]}')
 
 class Model():
     def __init__(self):
         self.networks = list()
+        self.hosts = list()
 
-    def incomplete_networks(self):
-        return [nw for nw in self.networks if len(nw.associated_networks) < nw.n_associated_networks]
+    def incompletely_associated(self, source_associated_asset_type: str, target_associated_asset_type: str):
+        if source_associated_asset_type == 'network':
+            return [nw for nw in self.networks if not nw.fully_associated(target_associated_asset_type)]
+        if source_associated_asset_type == 'host':
+            return [h for h in self.hosts if not h.fully_associated(target_associated_asset_type)]
+        raise ValueError(f'Unknown source associated asset type: {source_associated_asset_type}')
 
     def find_or_create_neighbor(self, source_network: Network):
-        print(f'Total number of networks: {len(self.networks)} of which {len(self.incomplete_networks())} are incomplete')
-        print(f'Finding neighbor for network {source_network.name}, currently associated with {len(source_network.associated_networks)} networks and aiming for {source_network.n_associated_networks} associated networks')
         assert source_network.n_associated_networks < len(self.networks)
-        inc_net =  self.incomplete_networks()
+        inc_net =  self.incompletely_associated('network', 'network')
         available_networks = [nw for nw in inc_net if nw not in source_network.associated_networks and nw != source_network]
-        print(f'Number of available networks: {len(available_networks)}', flush=True)
         if len(available_networks) == 0:
             nw = Network(f'Network_{len(self.networks)}')
-            print(f'Creating new network: {nw.name}')
             self.networks.append(nw)
             return nw
         return random.choice(available_networks)
@@ -62,5 +100,7 @@ class Model():
         self.print()
 
 if __name__ == "__main__":
-    model = Model()
-    model.sample()
+#    model = Model()
+#    model.sample()
+    p = binom(n=20, p=0.5).pmf(10)
+    print(p)
