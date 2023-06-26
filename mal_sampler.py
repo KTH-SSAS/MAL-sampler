90adda13e62413996fe21f747b5bb4070cb32fb0
pontusj101 <pontusj@kth.se>
2023-06-26 06:08:27 +0000

    Networks and hosts.

--- mal_sampler.py
+++ mal_sampler.py
@@ -17,56 +17,78 @@ class Asset:
         print(self.name)
 
 class Network(Asset):
-    p_networks = binom(n=20, p=0.5)
     def __init__(self, name: str):
         super().__init__(name)
-        self.max_associated_networks = 10
-        self.p_associated_networks = binom(n=5, p=0.5)
-        self.p_unassociated_networks = binom(n=self.max_associated_networks, p=0.99)
-        self.associated_networks = set()
+        self.n_networks = 1 + binom(n=5, p=0.5).rvs()
+        self.networks = set()
+        self.n_hosts = 1 + binom(n=10, p=0.5).rvs()
+        self.hosts = set()
 
-    def probability_of_another_network(self):
-        return 1 - self.p_associated_networks.cdf(len(self.associated_networks))
+    def accepts_network(self):
+        return len(self.networks) < self.n_networks
 
-    def spawn_network(self):
-        print(f'Spawning new network from {self.name}')
-        nw = Network(f'Network_{len(self.associated_networks)}_neigbor')
-        self.associated_networks.add(nw)
-        nw.associated_networks.add(self)
-        return nw
-
-    def veto_spawn_network(self):
-        return random.random() > self.probability_of_another_network()
+    def accepts_host(self):
+        return len(self.hosts) < self.n_hosts
 
     def print(self):
-        print(f'Network: {self.name}. Associated networks: {[an.name for an in self.associated_networks]}')
+        print(f'Network: {self.name}. Associated networks: {[an.name for an in self.networks]}. Associated hosts: {[ah.name for ah in self.hosts]}')
+
+
+class Host(Asset):
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.n_networks = 1
+        self.networks = set()
+
+    def accepts_network(self):
+        return len(self.networks) < self.n_networks
+
+    def print(self):
+        print(f'Host: {self.name}. Networks: {[nw.name for nw in self.networks]}')
+
 
 class Model():
     def __init__(self):
-        self.networks = list()
-        self.hosts = list()
-
-    def veto_add_network(self):
-        return random.random() < Network.p_networks.cdf(len(self.networks))
+        self.n_networks = 1 + binom(n=30, p=0.5).rvs()
+        self.networks = set()
+        self.hosts = set()
 
     def add_network(self):
-        if self.veto_add_network():
-            print(f'Vetoing add network. Number of networks: {len(self.networks)}')
-            return None
+        if len(self.networks) <= self.n_networks:
+            nw = Network(f'Network_{len(self.networks)}')
+            self.networks.add(nw)
         else:
-            if len(self.networks) == 0:
-                nw = Network(f'Network_{len(self.networks)}')
-                self.networks.append(nw)
-                return nw
+            print('Cannot add more networks')
+        return self.n_networks - len(self.networks)
+ 
+    def associate_networks_to_networks(self):
+        available_networks = [nw for nw in self.networks if nw.accepts_network()]
+        if len(available_networks) < 2:
+            print('Not enough networks to associate to each other')
+            return len(available_networks)
+        else:
+            source_nw = random.choice(available_networks)
+            target_nw = random.choice([nw for nw in available_networks if nw != source_nw])
+            if source_nw in target_nw.networks or target_nw in source_nw.networks:
+                print('Networks already associated')
+                return len(available_networks) - 1
             else:
-                source_nw = random.choice(self.networks)
-                if source_nw.veto_spawn_network():
-                    print(f'{source_nw.name} is vetoing spawn network. Number of associated networks: {len(source_nw.associated_networks)}')
-                    return None
-                else:
-                    target_nw = source_nw.spawn_network()
-                    self.networks.append(target_nw)
-                    return target_nw
+                source_nw.networks.add(target_nw)
+                target_nw.networks.add(source_nw)
+                print(f'Associated {source_nw.name} to {target_nw.name}. {len(available_networks)} networks left')
+                return len(available_networks)
+
+    def associate_hosts_to_networks(self):
+        available_networks = [nw for nw in self.networks if nw.accepts_host()]
+        if len(available_networks) == 0:
+            print('Not enough networks to associate hosts to')
+        else:
+            nw = random.choice(available_networks)
+            host = Host(f'Host_{len(self.hosts)}')
+            self.hosts.add(host)
+            nw.hosts.add(host)
+            host.networks.add(nw)
+        return len(available_networks)
 
     def print(self):
         for nw in self.networks:
@@ -78,16 +100,17 @@ class Sampler():
         self.current_model = Model()
 
     def sample(self):
-        self.current_model.add_network()
+        print('Adding networks')
+        while self.current_model.add_network() > 0:
+            pass
+        print('Associating networks to networks')
+        while self.current_model.associate_networks_to_networks() > 1:
+            pass
+        print('Associating hosts to networks')
+        while self.current_model.associate_hosts_to_networks() > 0:
+            pass
         self.current_model.print()
-        self.current_model.add_network()
-        self.current_model.print()
-        self.current_model.add_network()
-        self.current_model.print()
-        self.current_model.add_network()
-        self.current_model.print()
-
-
+ 
 if __name__ == "__main__":
     sampler = Sampler()
     sampler.sample()
