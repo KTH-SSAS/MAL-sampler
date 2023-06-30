fce7e313f053be5971b2eeb0c3157fd5a2621998
pontusj101 <pontusj@kth.se>
2023-06-30 07:18:37 +0000

    All good. About to develop consistency generator.

--- mal_sampler.py
+++ mal_sampler.py
@@ -29,7 +29,8 @@ class Asset:
         self.associated_assets = dict()
         self.generation_completed = False
         for asset_name, dist_dict in associated_assets_dict.items():
-            self.n_associated_assets[asset_name] = ProbabilityDistribution(dist_dict).sample()
+            self.n_associated_assets[asset_name] = ProbabilityDistribution(
+                dist_dict).sample()
             self.associated_assets[asset_name] = set()
 
     def accepts(self, asset_type: str):
@@ -50,7 +51,8 @@ class Model:
         self.metamodel = metamodel
         for asset_type in self.metamodel:
             if 'n' in self.metamodel[asset_type]:
-                self.n_assets[asset_type] = ProbabilityDistribution(self.metamodel[asset_type]['n']).sample()
+                self.n_assets[asset_type] = ProbabilityDistribution(
+                    self.metamodel[asset_type]['n']).sample()
             else:
                 self.n_assets[asset_type] = sys.maxsize
             self.assets[asset_type] = set()
@@ -66,54 +68,52 @@ class Model:
     def incompletely_associated_assets(self):
         return [a for a in self.all_assets() if not a.generation_completed]
 
-    def find_available_targets(self, source_asset, target_asset_type):
-        # 
+    def available_targets(self, source_asset, target_asset_type):
         available_target_assets = [
-            a for a in self.assets[target_asset_type] if a.accepts(source_asset.asset_type_name)]
-        available_target_assets = [
-            a for a in available_target_assets if a not in source_asset.associated_assets[target_asset_type]]
+            a for a in self.assets[target_asset_type] if a.accepts(source_asset.asset_type_name) and a not in source_asset.associated_assets[target_asset_type]]
         if target_asset_type == source_asset.asset_type_name:
             available_target_assets = [
-                a for a in available_target_assets if a != source_asset]         
+                a for a in available_target_assets if a != source_asset]
         return available_target_assets
 
     def add(self, asset_type: str):
         if asset_type in self.metamodel:
-            abbreviation = self.metamodel[asset_type]['abbreviation']
             a = Asset(
-                f"{abbreviation}{len(self.assets[asset_type])}", asset_type, self.metamodel[asset_type]['associated_assets'])
+                f"{self.metamodel[asset_type]['abbreviation']}{len(self.assets[asset_type])}", asset_type, self.metamodel[asset_type]['associated_assets'])
+            self.assets[asset_type].add(a)
+            return a
         else:
             raise ValueError(f'Unknown asset type: {asset_type}')
-        self.assets[asset_type].add(a)
-        return a
 
     def associate(self, source_asset, target_asset_type, source_asset_type, target_asset):
         source_asset.associated_assets[target_asset_type].add(
-                target_asset)
+            target_asset)
         if source_asset_type not in target_asset.associated_assets:
             target_asset.associated_assets[source_asset_type] = set()
         target_asset.associated_assets[source_asset_type].add(
-                source_asset)
+            source_asset)
 
     def complete_associations(self, source_asset: Asset):
         for target_asset_type in source_asset.n_associated_assets.keys():
             while source_asset.accepts(target_asset_type):
-                available_target_assets = self.find_available_targets(source_asset, target_asset_type)
                 target_asset = None
+                available_target_assets = self.available_targets(
+                    source_asset, target_asset_type)
                 if len(available_target_assets) > 0:
                     target_asset = random.choice(available_target_assets)
                 else:
                     if len(self.assets[target_asset_type]) < self.n_assets[target_asset_type]:
                         target_asset = self.add(target_asset_type)
                 if target_asset:
-                    self.associate(source_asset, target_asset_type, source_asset.asset_type_name, target_asset)
+                    self.associate(source_asset, target_asset_type,
+                                   source_asset.asset_type_name, target_asset)
                 else:
                     break
         source_asset.generation_completed = True
 
     def sample(self):
         initial_asset_type = self.select_initial_asset_type()
-        asset = self.add(initial_asset_type)        
+        asset = self.add(initial_asset_type)
         self.complete_associations(asset)
         while self.incompletely_associated_assets():
             asset = random.choice(self.incompletely_associated_assets())
@@ -124,7 +124,8 @@ class Model:
         for asset in self.all_assets():
             for associated_asset_type in asset.n_associated_assets.keys():
                 if len(asset.associated_assets[associated_asset_type]) != asset.n_associated_assets[associated_asset_type]:
-                    print(f'Inconsistency: Asset {asset.name} has {len(asset.associated_assets[associated_asset_type])} associated {associated_asset_type} assets of {asset.n_associated_assets[associated_asset_type]} required associated assets.')
+                    print(
+                        f'Inconsistency: Asset {asset.name} has {len(asset.associated_assets[associated_asset_type])} associated {associated_asset_type} assets of {asset.n_associated_assets[associated_asset_type]} required associated assets.')
                     is_consistent = False
         return is_consistent
 
@@ -154,10 +155,6 @@ class Model:
         plt.axis('off')
         plt.savefig('model.png', dpi=1200, bbox_inches='tight',
                     pad_inches=0, facecolor='black')
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -221,66 +218,65 @@ if __name__ == "__main__":
                                                          'p': 0.005}},
                                      'visualization': {'shape': 's',
                                                        'color': 'red'}},
-                         'vm_instance': {'abbreviation': 'VM',
-                                         'associated_assets': {
-                                             'network': {'distribution': 'Constant',
-                                                         'n': 1},
-                                             'admin_privileges': {'distribution': 'Constant',
-                                                                  'n': 1}},
-                                         'visualization': {'shape': 'o',
-                                                           'color': 'blue'}},
-                         'admin_privileges': {'abbreviation': 'A',
-                                              'associated_assets': {
-                                                  'vm_instance': {'distribution': 'Constant',
-                                                                  'n': 1},
-                                                  'principal': {'distribution': 'BinomialPlusOne',
-                                                                'n': 5,
-                                                                'p': 0.1}},
-                                              'visualization': {'shape': 'v',
-                                                                'color': 'yellow'}},
-                         'principal': {'abbreviation': 'P',
-                                       'associated_assets': {
-                                           'admin_privileges': {'distribution': 'Binomial',
-                                                                'n': 200,
-                                                                'p': 0.1}},
-                                       'visualization': {'shape': '^',
-                                                         'color': 'green'}}}
+                                     'vm_instance': {'abbreviation': 'VM',
+                                                     'associated_assets': {
+                                                         'network': {'distribution': 'Constant',
+                                                                     'n': 1},
+                                                         'admin_privileges': {'distribution': 'Constant',
+                                                                              'n': 1}},
+                                                     'visualization': {'shape': 'o',
+                                                                       'color': 'blue'}},
+                                     'admin_privileges': {'abbreviation': 'A',
+                                                          'associated_assets': {
+                                                              'vm_instance': {'distribution': 'Constant',
+                                                                              'n': 1},
+                                                              'principal': {'distribution': 'BinomialPlusOne',
+                                                                            'n': 5,
+                                                                            'p': 0.1}},
+                                                          'visualization': {'shape': 'v',
+                                                                            'color': 'yellow'}},
+                                     'principal': {'abbreviation': 'P',
+                                                   'associated_assets': {
+                                                       'admin_privileges': {'distribution': 'Binomial',
+                                                                            'n': 200,
+                                                                            'p': 0.1}},
+                                                   'visualization': {'shape': '^',
+                                                                     'color': 'green'}}}
 
     minimal_constant_metamodel = {'network': {'abbreviation': 'N',
-                                     'n': {'distribution': 'Constant',
-                                           'n': 3},
-                                     'associated_assets': {
-                                         'network': {'distribution': 'Constant',
-                                                     'n': 2},
-                                         'vm_instance': {'distribution': 'Constant',
-                                                         'n': 3}},
-                                     'visualization': {'shape': 's',
-                                                       'color': 'red'}},
-                         'vm_instance': {'abbreviation': 'VM',
-                                         'associated_assets': {
-                                             'network': {'distribution': 'Constant',
-                                                         'n': 1},
-                                             'admin_privileges': {'distribution': 'Constant',
-                                                                  'n': 1}},
-                                         'visualization': {'shape': 'o',
-                                                           'color': 'blue'}},
-                         'admin_privileges': {'abbreviation': 'A',
+                                              'n': {'distribution': 'Constant',
+                                                    'n': 3},
                                               'associated_assets': {
+                                                  'network': {'distribution': 'Constant',
+                                                              'n': 2},
                                                   'vm_instance': {'distribution': 'Constant',
+                                                                  'n': 3}},
+                                              'visualization': {'shape': 's',
+                                                                'color': 'red'}},
+                                  'vm_instance': {'abbreviation': 'VM',
+                                                  'associated_assets': {
+                                                      'network': {'distribution': 'Constant',
                                                                   'n': 1},
-                                                  'principal': {'distribution': 'Constant',
-                                                                'n': 1}},
-                                              'visualization': {'shape': 'v',
-                                                                'color': 'yellow'}},
-                         'principal': {'abbreviation': 'P',
-                                       'associated_assets': {
-                                           'admin_privileges': {'distribution': 'Constant',
-                                                                'n': 2}},
-                                       'visualization': {'shape': '^',
-                                                         'color': 'green'}}}
+                                                      'admin_privileges': {'distribution': 'Constant',
+                                                                           'n': 1}},
+                                                  'visualization': {'shape': 'o',
+                                                                    'color': 'blue'}},
+                                  'admin_privileges': {'abbreviation': 'A',
+                                                       'associated_assets': {
+                                                           'vm_instance': {'distribution': 'Constant',
+                                                                           'n': 1},
+                                                           'principal': {'distribution': 'Constant',
+                                                                         'n': 1}},
+                                                       'visualization': {'shape': 'v',
+                                                                         'color': 'yellow'}},
+                                  'principal': {'abbreviation': 'P',
+                                                'associated_assets': {
+                                                    'admin_privileges': {'distribution': 'Constant',
+                                                                         'n': 2}},
+                                                'visualization': {'shape': '^',
+                                                                  'color': 'green'}}}
 
-    model = Model(minimal_constant_metamodel)
+    model = Model(probably_self_contradictory_metamodel)
     model.sample()
     model.plot()
     model.check_consistency()
-
