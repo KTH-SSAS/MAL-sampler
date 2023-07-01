6e09299cec6237d2604bee89b1f106c7ecbc79ab
pontusj101 <pontusj@kth.se>
2023-07-01 06:43:10 +0000

    Consistency enforcer seems to work!

--- mal_sampler.py
+++ mal_sampler.py
@@ -6,12 +6,16 @@ import sys
 
 
 class ProbabilityDistribution:
-    def __init__(self, distribution_dict: dict):
+    def __init__(self, distribution_dict: dict, n_samples_for_bounds: int = 100):
         self.distribution = distribution_dict['distribution']
         self.n = distribution_dict['n']
         if 'p' in distribution_dict:
             self.p = distribution_dict['p']
-        self.value = self.sample()
+        samples = [self.sample() for _ in range(n_samples_for_bounds)]
+        sorted_samples = sorted(samples)
+        self.value = samples[0]
+        self.low = sorted_samples[0]
+        self.high = sorted_samples[-1]
 
     def sample(self):
         if self.distribution == 'Binomial':
@@ -33,11 +37,14 @@ class Asset:
                 dist_dict)
             self.associated_assets[asset_name] = set()
 
-    def accepts(self, asset_type: str):
+    def accepts(self, asset_type: str, force_accept=False):
         if asset_type not in self.n_associated_assets:
             return False
         else:
-            return len(self.associated_assets[asset_type]) < self.n_associated_assets[asset_type].value
+            if not force_accept:
+                return len(self.associated_assets[asset_type]) < self.n_associated_assets[asset_type].value
+            else:
+                return len(self.associated_assets[asset_type]) < self.n_associated_assets[asset_type].high
 
     def associate(self, target):
         self.associated_assets[target.asset_type_name].add(target)
@@ -83,9 +90,9 @@ class Model:
     def incompletely_associated_assets(self):
         return [a for a in self.all_assets() if not a.generation_completed]
 
-    def available_targets(self, source_asset, target_asset_type):
+    def available_targets(self, source_asset, target_asset_type, force_associate=False):
         available_target_assets = [
-            a for a in self.assets[target_asset_type] if a.accepts(source_asset.asset_type_name) and a not in source_asset.associated_assets[target_asset_type]]
+            a for a in self.assets[target_asset_type] if a.accepts(source_asset.asset_type_name, force_accept=force_associate) and a not in source_asset.associated_assets[target_asset_type]]
         if target_asset_type == source_asset.asset_type_name:
             available_target_assets = [
                 a for a in available_target_assets if a != source_asset]
@@ -105,17 +112,21 @@ class Model:
         if asset in self.assets[asset.asset_type_name]:
             self.assets[asset.asset_type_name].remove(asset)
 
-    def complete_associations(self, source_asset: Asset):
+    def complete_associations(self, source_asset: Asset, force_associate=False):
         for target_asset_type in source_asset.n_associated_assets.keys():
             while source_asset.accepts(target_asset_type):
                 target_asset = None
                 available_target_assets = self.available_targets(
-                    source_asset, target_asset_type)
+                    source_asset, target_asset_type, force_associate=force_associate)
                 if len(available_target_assets) > 0:
                     target_asset = random.choice(available_target_assets)
+                    if force_associate:
+                        print(f'Forced association between {source_asset.asset_type_name} {source_asset.name} and {target_asset.asset_type_name} {target_asset.name}')
                 else:
                     if len(self.assets[target_asset_type]) < self.n_assets[target_asset_type].value:
                         target_asset = self.add(target_asset_type)
+                        if force_associate:
+                            print(f'Forced creation of new asset of type {target_asset_type}')
                 if target_asset:
                     source_asset.associate(target_asset)
                 else:
@@ -134,8 +145,9 @@ class Model:
         inconsistent_assets = []
         for asset in self.all_assets():
             for associated_asset_type in asset.n_associated_assets.keys():
-                if len(asset.associated_assets[associated_asset_type]) != asset.n_associated_assets[associated_asset_type].value:
+                if len(asset.associated_assets[associated_asset_type]) < asset.n_associated_assets[associated_asset_type].low or len(asset.associated_assets[associated_asset_type]) > asset.n_associated_assets[associated_asset_type].high:
                     inconsistent_assets.append(asset)
+                    print(f'Inconsistent asset: {asset.name} ({asset.asset_type_name}): {len(asset.associated_assets[associated_asset_type])} associated {associated_asset_type} assets, targeted  {asset.n_associated_assets[associated_asset_type].value} and accepts between {asset.n_associated_assets[associated_asset_type].low} and {asset.n_associated_assets[associated_asset_type].high}')
         return inconsistent_assets
 
     def print(self):
@@ -143,7 +155,7 @@ class Model:
             for asset in self.assets[asset_type]:
                 asset.print()
 
-    def plot(self):
+    def plot(self, filename: str):
         G = nx.Graph()
         for asset_type_name in self.assets.keys():
             for asset in self.assets[asset_type_name]:
@@ -162,7 +174,7 @@ class Model:
         nx.draw_networkx_edges(G, pos, edge_color='white', width=0.5)
         nx.draw_networkx_labels(G, pos, font_color='white', font_size=2)
         plt.axis('off')
-        plt.savefig('model.png', dpi=1200, bbox_inches='tight',
+        plt.savefig(filename, dpi=1200, bbox_inches='tight',
                     pad_inches=0, facecolor='black')
 
 
@@ -285,16 +297,66 @@ if __name__ == "__main__":
                                                 'visualization': {'shape': '^',
                                                                   'color': 'green'}}}
 
+    minimal_probabilistic_metamodel = {'network': {'abbreviation': 'N',
+                                              'n': {'distribution': 'BinomialPlusOne',
+                                                    'n': 3,
+                                                    'p': 0.5},
+                                              'associated_assets': {
+                                                  'network': {'distribution': 'BinomialPlusOne',
+                                                              'n': 2,
+                                                              'p': 0.5},
+                                                  'vm_instance': {'distribution': 'BinomialPlusOne',
+                                                                  'n': 30,
+                                                                  'p': 0.05}},
+                                              'visualization': {'shape': 's',
+                                                                'color': 'red'}},
+                                  'vm_instance': {'abbreviation': 'VM',
+                                                  'associated_assets': {
+                                                      'network': {'distribution': 'Constant',
+                                                                  'n': 1},
+                                                      'admin_privileges': {'distribution': 'Constant',
+                                                                           'n': 1}},
+                                                  'visualization': {'shape': 'o',
+                                                                    'color': 'blue'}},
+                                  'admin_privileges': {'abbreviation': 'A',
+                                                       'associated_assets': {
+                                                           'vm_instance': {'distribution': 'Constant',
+                                                                           'n': 1},
+                                                           'principal': {'distribution': 'BinomialPlusOne',
+                                                                         'n': 3,
+                                                                         'p': 0.5}},
+                                                       'visualization': {'shape': 'v',
+                                                                         'color': 'yellow'}},
+                                  'principal': {'abbreviation': 'P',
+                                                'associated_assets': {
+                                                    'admin_privileges': {'distribution': 'BinomialPlusOne',
+                                                                         'n': 3,
+                                                                         'p': 0.5}},
+                                                'visualization': {'shape': '^',
+                                                                  'color': 'green'}}}
+
+
     model = Model(probably_self_contradictory_metamodel)
     model.sample()
     inconsistent_assets = model.check_consistency()
     print(f'Assets: {len(model.all_assets())}.')
     print(f'Inconsistent assets: {len(inconsistent_assets)}')
-    # while inconsistent_assets:
-    #     for inconsistent_asset in inconsistent_assets:
-    #         model.remove(inconsistent_asset)
-    #     print(f'Removed {inconsistent_asset.name} from the model. Remaining assets: {len(model.all_assets())}.')
-    #     inconsistent_assets = model.check_consistency()
-    #     print(f'Remaining inconsistent assets: {len(inconsistent_assets)}')
-    model.plot()
+    model.plot('model_1.png')
+    counter = 0
+    while inconsistent_assets:
+        counter += 1
+        for inconsistent_asset in inconsistent_assets:
+            print(f'Attempting to force association for {inconsistent_asset.name}.')
+            model.complete_associations(inconsistent_asset, force_associate=True)
+        inconsistent_assets = model.check_consistency()
+        print(f'Remaining inconsistent assets: {len(inconsistent_assets)}')
+        model.plot('model_2.png')
+        for inconsistent_asset in inconsistent_assets:
+            model.remove(inconsistent_asset)
+        print(f'Removed {inconsistent_asset.name} from the model. Remaining assets: {len(model.all_assets())}.')
+        inconsistent_assets = model.check_consistency()
+        print(f'Remaining inconsistent assets: {len(inconsistent_assets)}')
+        model.plot('model_3.png')
+        if counter > 10:
+            break
 
