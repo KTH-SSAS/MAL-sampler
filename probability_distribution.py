1b11eeec74782304a37600fcb60aa7e2e6953331
pontusj101 <pontusj@kth.se>
2023-07-01 08:27:33 +0000

    ProbabilityDistribution to separate file.

---
+++ probability_distribution.py
@@ -0,0 +1,21 @@
+from scipy.stats import binom
+
+class ProbabilityDistribution:
+    def __init__(self, distribution_dict: dict, n_samples_for_bounds: int):
+        self.distribution = distribution_dict['distribution']
+        self.n = distribution_dict['n']
+        if 'p' in distribution_dict:
+            self.p = distribution_dict['p']
+        samples = [self.sample() for _ in range(n_samples_for_bounds)]
+        sorted_samples = sorted(samples)
+        self.value = samples[0]
+        self.low = sorted_samples[0]
+        self.high = sorted_samples[-1]
+
+    def sample(self):
+        if self.distribution == 'Binomial':
+            return binom(n=self.n, p=self.p).rvs()
+        elif self.distribution == 'Constant':
+            return self.n
+        elif self.distribution == 'BinomialPlusOne':
+            return binom(n=self.n, p=self.p).rvs() + 1
